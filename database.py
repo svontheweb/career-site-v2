71e7909d34ada05db0f4664ea036f49cfb64bcd4
Sujeet <97327428+svontheweb@users.noreply.github.com>
2023-03-03 17:13:03 +0000

    QUERY table rows and print in console by >python database.py LEGARY version not work alternate method to zip() keys, and rows

--- database.py
+++ database.py
@@ -1,7 +1,30 @@
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+
 db_connection_string = os.environ['DB_CONNECTION_STRING']
-engine = create_engine(db_connection_string)
+ssl_args = {'ssl': {
+            'ssl_ca': "/etc/ssl/cert.pem"}
+          }
+engine = create_engine(db_connection_string,
+                      connect_args=ssl_args)
 
+# Connect to the database using 'with' statement
+with engine.connect() as conn:
+  result = conn.execute(text("select * from jobs"))
+  result_all = result.all()
+  column_names = result.keys() 
+  first_result = result_all[0]
+  first_result_dict = dict(zip(column_names, first_result))
+  print(first_result_dict)
 
-
+  # OR,
+# CREATE "LIST" OF all rows of database table with id
+with engine.connect() as conn:
+  result = conn.execute(text("select * from jobs"))
+  column_names = result.keys()
+    
+  result_dicts = []
+    
+  for row in result.all():
+    result_dicts.append(dict(zip(column_names, row)))
+  print(result_dicts)
